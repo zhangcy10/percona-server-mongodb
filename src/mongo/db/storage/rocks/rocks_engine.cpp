bd3059e72bd3f08c74cfa3fa7712ba0559601523
Igor Canadi <icanadi@fb.com>
2015-07-09 09:41:58 -0700

    Decrease block cache shard count to 2^6 from 2^7

--- src/mongo/db/storage/rocks/rocks_engine.cpp
+++ src/mongo/db/storage/rocks/rocks_engine.cpp
@@ -168,7 +168,7 @@ namespace mongo {
                     cacheSizeGB = 1;
                 }
             }
-            _block_cache = rocksdb::NewLRUCache(cacheSizeGB * 1024 * 1024 * 1024LL, 7);
+            _block_cache = rocksdb::NewLRUCache(cacheSizeGB * 1024 * 1024 * 1024LL, 6);
         }
         _maxWriteMBPerSec = rocksGlobalOptions.maxWriteMBPerSec;
         _rateLimiter.reset(
