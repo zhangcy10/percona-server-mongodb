zaorangyang/percona-server-mongodb-annotation

Replace TokuFT with PerconaFT in option names, error messages, logs etc...
Former-commit-id: c6c536afa6c8e49802e78183162677f1bc803ea2

src/mongo/db/storage/tokuft/tokuft_init.cpp
@@ -43,8 +43,8 @@ namespace mongo {
               _durable(durable)
         {
             if (!_durable) {
-                warning() << "TokuFT: Initializing with --nojournal.  Note that this will cause {j: true} writes to fail, but will not actually disable journaling.";
-                warning() << "TokuFT: This is only for tests, there is no reason to run with --nojournal in production.";
+                warning() << "PerconaFT: Initializing with --nojournal.  Note that this will cause {j: true} writes to fail, but will not actually disable journaling.";
+                warning() << "PerconaFT: This is only for tests, there is no reason to run with --nojournal in production.";
             }
         }
 
@@ -60,13 +60,13 @@ namespace mongo {
         virtual StorageEngine *create(const StorageGlobalParams &params,
                                       const StorageEngineLockFile &lockFile) const {
             if (params.directoryperdb) {
-                severe() << "TokuFT: directoryPerDB not yet supported.  This option is incompatible with TokuFT.";
-                severe() << "TokuFT: The following server crash is intentional.";
+                severe() << "PerconaFT: directoryPerDB not yet supported.  This option is incompatible with PerconaFT.";
+                severe() << "PerconaFT: The following server crash is intentional.";
                 fassertFailedNoTrace(28628);
             }
             if (tokuftGlobalOptions.engineOptions.directoryForIndexes) {
-                severe() << "TokuFT: directoryForIndexes not yet supported.  This option is incompatible with TokuFT.";
-                severe() << "TokuFT: The following server crash is intentional.";
+                severe() << "PerconaFT: directoryForIndexes not yet supported.  This option is incompatible with PerconaFT.";
+                severe() << "PerconaFT: The following server crash is intentional.";
                 fassertFailedNoTrace(28629);
             }
 

zhangcy10/percona-server-mongodb

Conflicts:
	src/mongo/db/storage/tokuft/tokuft_engine.cpp
	src/mongo/db/storage/tokuft/tokuft_engine_server_status.cpp
	src/mongo/db/storage/tokuft/tokuft_init.cpp

src/mongo/db/storage/tokuft/tokuft_init.cpp
@@ -43,8 +43,8 @@ namespace mongo {
               _durable(durable)
         {
             if (!_durable) {
-                warning() << "TokuFT: Initializing with --nojournal.  Note that this will cause {j: true} writes to fail, but will not actually disable journaling.";
-                warning() << "TokuFT: This is only for tests, there is no reason to run with --nojournal in production.";
+                warning() << "PerconaFT: Initializing with --nojournal.  Note that this will cause {j: true} writes to fail, but will not actually disable journaling.";
+                warning() << "PerconaFT: This is only for tests, there is no reason to run with --nojournal in production.";
             }
         }
 
@@ -60,13 +60,13 @@ namespace mongo {
         virtual StorageEngine *create(const StorageGlobalParams &params,
                                       const StorageEngineLockFile &lockFile) const {
             if (params.directoryperdb) {
-                severe() << "TokuFT: directoryPerDB not yet supported.  This option is incompatible with TokuFT.";
-                severe() << "TokuFT: The following server crash is intentional.";
+                severe() << "PerconaFT: directoryPerDB not yet supported.  This option is incompatible with TokuFT.";
+                severe() << "PerconaFT: The following server crash is intentional.";
                 fassertFailedNoTrace(28631);
             }
             if (tokuftGlobalOptions.engineOptions.directoryForIndexes) {
-                severe() << "TokuFT: directoryForIndexes not yet supported.  This option is incompatible with TokuFT.";
-                severe() << "TokuFT: The following server crash is intentional.";
+                severe() << "PerconaFT: directoryForIndexes not yet supported.  This option is incompatible with TokuFT.";
+                severe() << "PerconaFT: The following server crash is intentional.";
                 fassertFailedNoTrace(28630);
             }
 