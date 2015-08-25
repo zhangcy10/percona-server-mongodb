7486147c8f2c2691c16eb0ab6273fcb317bd9469
Igor Solodovnikov <igor.solodovnikov@percona.com>
2015-08-25 14:19:08 +0300

    Implemented File class extension to allow audit code to handle IO errors
    (cherry picked from commit 355d0d179a8d3bad1037a1f0aae06f1141317c1f)

---
+++ src/mongo/db/audit/audit_file.h
@@ -0,0 +1,43 @@
+/* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+
+/*======
+This file is part of Percona Server for MongoDB.
+
+Copyright (c) 2006, 2015, Percona and/or its affiliates. All rights reserved.
+
+    Percona Server for MongoDB is free software: you can redistribute
+    it and/or modify it under the terms of the GNU Affero General
+    Public License, version 3, as published by the Free Software
+    Foundation.
+
+    Percona Server for MongoDB is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with Percona Server for MongoDB.  If not, see
+    <http://www.gnu.org/licenses/>.
+======= */
+
+#pragma once
+
+#include "mongo/util/file.h"
+
+
+namespace mongo {
+
+namespace audit {
+
+    // Default File implementation ignores possible errors
+    // So we need to implement this extension
+    class AuditFile : public File {
+    public:
+        int fsyncReturningError() const;
+        int writeReturningError(fileofs o, const char *data, unsigned len);
+    };
+
+}  // namespace audit
+}  // namespace mongo
+
