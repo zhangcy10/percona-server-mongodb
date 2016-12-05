f50b5d6c50faf0abfa7bf682990d3515329b4d84
Tim Vaillancourt <tim@elementspace.com>
2016-12-05 18:36:25 +0100

    Disable THP on all storage engine types, as per MongoDB Production Notes and shell warnings

--- percona-packaging/conf/percona-server-mongodb-helper.sh
+++ percona-packaging/conf/percona-server-mongodb-helper.sh
@@ -31,13 +31,9 @@ else
     DAEMON_OPTS=${DAEMON_OPTS:-"--config $CONF"}
 fi
 #
-# checking if PerconaFT is used looking at defaults file and daemon config
-defaults=$(echo "${OPTIONS}" | egrep -o 'storageEngine.*PerconaFT' | tr -d '[[:blank:]]' | awk -F'=' '{print $NF}' 2>/dev/null)
-config=$(egrep -o '^[[:blank:]]+engine.*PerconaFT' ${CONF} | tr -d '[[:blank:]]' | awk -F':' '{print $NF}' 2>/dev/null)
-#
-if [ -z "${defaults}" ] && [ -z "${config}" ]; then # nothing to do
-  exit 0
-fi
+# checking if storageEngine is defined twice (in default and config file)
+defaults=$(echo "${OPTIONS}" | egrep -o 'storageEngine.*' | tr -d '[[:blank:]]' | awk -F'=' '{print $NF}' 2>/dev/null)
+config=$(egrep -o '^[[:blank:]]+engine.*' ${CONF} | tr -d '[[:blank:]]' | awk -F':' '{print $NF}' 2>/dev/null)
 #
 if [ -n "${defaults}" ] && [ -n "${config}" ]; then # engine is set in 2 places
   if [ "${defaults}" ==  "${config}" ]; then # it's OK
@@ -47,14 +43,6 @@ if [ -n "${defaults}" ] && [ -n "${config}" ]; then # engine is set in 2 places
     exit 1
   fi
 fi
-#
-if [ -n "${defaults}" ]; then
-  storageEngine=${defaults}
-else
-  storageEngine=${config}
-fi
-# trying to disable THP only if PerconaFT engine is enabled and THP is set to always
-if [ "${storageEngine}" = PerconaFT ]; then
-  fgrep '[always]' ${KTHP}/enabled  > /dev/null 2>&1 && (echo never > ${KTHP}/enabled 2> /dev/null || print_error) || true
-  fgrep '[always]' ${KTHP}/defrag   > /dev/null 2>&1 && (echo never > ${KTHP}/defrag  2> /dev/null || print_error) || true
-fi
+# disable THP
+fgrep '[always]' ${KTHP}/enabled  > /dev/null 2>&1 && (echo never > ${KTHP}/enabled 2> /dev/null || print_error) || true
+fgrep '[always]' ${KTHP}/defrag   > /dev/null 2>&1 && (echo never > ${KTHP}/defrag  2> /dev/null || print_error) || true
