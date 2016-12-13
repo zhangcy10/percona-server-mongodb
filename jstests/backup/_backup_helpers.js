ecfa9c1a6015421c8245d8347d47d568bbfd6171
Denis Protyvenskyi <denis.protyvenskyi@percona.com>
2016-12-13 14:30:06 +0300

    PSMDB-98 Create dest folder for backup if it doesn't exist
    Also don't check if this folder is empty -
    just perform the backup if possible.
    
    (cherry picked from commit c1c0b1e553a6827fcbab363ebaf33a60ab6034a4)

--- jstests/backup/_backup_helpers.js
+++ jstests/backup/_backup_helpers.js
@@ -26,9 +26,10 @@ function computeHashes(conn) {
     return hashes;
 }
 
-function backup(conn) {
+function backup(conn, createDest = false) {
     var backupPath = MongoRunner.dataPath + 'backup';
-    mkdir(backupPath);
+    if (createDest)
+        mkdir(backupPath);
     var adminDB = conn.getDB('admin');
     assert.commandWorked(adminDB.runCommand({createBackup: 1, backupDir: backupPath}));
     return backupPath;
