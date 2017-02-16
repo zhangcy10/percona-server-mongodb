87df5da1a155e2e5af756cb11e0aee1719f39211
Denis Protyvenskyi <denis.protyvenskyi@percona.com>
2017-02-16 11:29:05 +0300

    Revert "SERVER-27300 delete dead function shouldUseCollationIndexKey()"
    This reverts commit 516bd8ab5eb1f2f0a377d5b4cb3efc78a259018d.

--- src/mongo/db/query/collation/collation_index_key_test.cpp
+++ src/mongo/db/query/collation/collation_index_key_test.cpp
@@ -39,25 +39,31 @@ namespace {
 
 using namespace mongo;
 
-TEST(CollationIndexKeyTest, IsCollatableTypeShouldBeTrueForString) {
+TEST(CollationIndexKeyTest, ShouldUseCollationIndexKeyFalseWithNullCollator) {
     BSONObj obj = BSON("foo"
                        << "string");
-    ASSERT_TRUE(CollationIndexKey::isCollatableType(obj.firstElement().type()));
+    ASSERT_FALSE(CollationIndexKey::shouldUseCollationIndexKey(obj.firstElement(), nullptr));
 }
 
-TEST(CollationIndexKeyTest, IsCollatableTypeShouldBeTrueForObject) {
-    BSONObj obj = BSON("foo" << BSON("bar" << 99));
-    ASSERT_TRUE(CollationIndexKey::isCollatableType(obj.firstElement().type()));
+TEST(CollationIndexKeyTest, ShouldUseCollationIndexKeyTrueWithObjectElement) {
+    CollatorInterfaceMock collator(CollatorInterfaceMock::MockType::kReverseString);
+    BSONObj obj = BSON("foo" << BSON("bar"
+                                     << "string"));
+    ASSERT_TRUE(CollationIndexKey::shouldUseCollationIndexKey(obj.firstElement(), &collator));
 }
 
-TEST(CollationIndexKeyTest, IsCollatableTypeShouldBeTrueForArray) {
-    BSONObj obj = BSON("foo" << BSON_ARRAY(98 << 99));
-    ASSERT_TRUE(CollationIndexKey::isCollatableType(obj.firstElement().type()));
+TEST(CollationIndexKeyTest, ShouldUseCollationIndexKeyTrueWithArrayElement) {
+    CollatorInterfaceMock collator(CollatorInterfaceMock::MockType::kReverseString);
+    BSONObj obj = BSON("foo" << BSON_ARRAY("one"
+                                           << "two"));
+    ASSERT_TRUE(CollationIndexKey::shouldUseCollationIndexKey(obj.firstElement(), &collator));
 }
 
-TEST(CollationIndexKeyTest, IsCollatableTypeShouldBeFalseForNumber) {
-    BSONObj obj = BSON("foo" << 99);
-    ASSERT_FALSE(CollationIndexKey::isCollatableType(obj.firstElement().type()));
+TEST(CollationIndexKeyTest, ShouldUseCollationIndexKeyTrueWithStringElement) {
+    CollatorInterfaceMock collator(CollatorInterfaceMock::MockType::kReverseString);
+    BSONObj obj = BSON("foo"
+                       << "string");
+    ASSERT_TRUE(CollationIndexKey::shouldUseCollationIndexKey(obj.firstElement(), &collator));
 }
 
 TEST(CollationIndexKeyTest, CollationAwareAppendCorrectlyAppendsElementWithNullCollator) {
