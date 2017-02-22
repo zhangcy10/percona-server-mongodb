73c74107e71fa027b50eb76954b7abd56cc2cc36
Denis Protyvenskyi <denis.protyvenskyi@percona.com>
2017-02-22 10:06:45 +0300

    PSMDB-123 Add WT-specific backup tests with directories in data dir
    When directoryperdb or wiredTigerDirectoryForIndexes (or both) is set,
    subdirectories are created in the data dir which may break the backup
    as we need to first create those subdirectories in the destination folder.

---
+++ jstests/backup/backup_wt_forindexes.js
@@ -0,0 +1,41 @@
+load('jstests/backup/_backup_helpers.js');
+
+(function() {
+    'use strict';
+
+    // Grab the storage engine, default is wiredTiger
+    var storageEngine = jsTest.options().storageEngine || "wiredTiger";
+    if (storageEngine !== "wiredTiger") {
+        jsTest.log('Skipping test because storageEngine is not "wiredTiger"');
+        return;
+    }
+
+    // Run the original instance and fill it with data.
+    var dbPath = MongoRunner.dataPath + 'original';
+    var conn = MongoRunner.runMongod({
+        dbpath: dbPath,
+        wiredTigerDirectoryForIndexes: '',
+    });
+
+    fillData(conn);
+    var hashesOrig = computeHashes(conn);
+
+    // Create backup.
+    var backupPath = backup(conn);
+    // Ensure we can still write after backup has finished
+    // and the data written doesn't affect backed up values.
+    fillData(conn, 500);
+    MongoRunner.stopMongod(conn);
+
+    // Run the backup instance.
+    var conn = MongoRunner.runMongod({
+        dbpath: backupPath,
+        wiredTigerDirectoryForIndexes: '',
+        noCleanData: true,
+    });
+
+    var hashesBackup = computeHashes(conn);
+    assert.hashesEq(hashesOrig, hashesBackup);
+
+    MongoRunner.stopMongod(conn);
+})();
