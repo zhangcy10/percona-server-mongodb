54ecd68cc1136acc0d46c3b5de859bbcb2000893
Igor Solodovnikov <igor.solodovnikov@percona.com>
2019-01-25 20:51:50 +0200

    PSMDB-257 add startup parameter to control permission checks behavior

--- src/mongo/db/server_options_helpers.cpp
+++ src/mongo/db/server_options_helpers.cpp
@@ -377,6 +377,14 @@ Status addGeneralServerOptions(moe::OptionSection* options) {
                                moe::Int,
                                unixSockPermsBuilder.str());
 
+    options
+        ->addOptionChaining("security.relaxPermChecks",
+                            "relaxPermChecks",
+                            moe::Switch,
+                            "allow group read and write flags to be set for files specified with"
+                            " --keyFile and --encryptionKeyFile")
+        .hidden();
+
     options->addOptionChaining(
         "processManagement.fork", "fork", moe::Switch, "fork server process");
 
@@ -955,6 +963,10 @@ Status storeServerOptions(const moe::Environment& params) {
     if (params.count("net.unixDomainSocket.filePermissions")) {
         serverGlobalParams.unixSocketPermissions =
             params["net.unixDomainSocket.filePermissions"].as<int>();
+    }
+    if (params.count("security.relaxPermChecks") &&
+        params["security.relaxPermChecks"].as<bool>() == true) {
+        serverGlobalParams.relaxPermChecks = true;
     }
 
     if ((params.count("processManagement.fork") &&
